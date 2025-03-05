a8ce957ba3b275fb88102e29a81aed29e7d3f7d9
HaoBrother666 <1972781375@qq.com>
2025-03-05 15:46:10 +0800

    创建了管理端项目

--- ManagePetStore-SSM-Dev/src/main/java/org/csu/petstore/entity/Category.java
+++ ManagePetStore-SSM-Dev/src/main/java/org/csu/petstore/entity/Category.java
@@ -4,15 +4,13 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
-import lombok.Getter;
 
 @Data
 @TableName("category")
 public class Category
 {
-    @TableId(value="catid")
+    @TableId(value = "catid")
     private String categoryId;
-    @Getter
     private String name;
     @TableField("descn")
     private String description;
