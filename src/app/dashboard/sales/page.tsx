570165f6fc64631b79d96d85b9ac0d26911edac8
lauvAri <198719993@qq.com>
2025-03-06 10:10:27 +0800

    add chart

--- src/app/dashboard/sales/page.tsx
+++ src/app/dashboard/sales/page.tsx
@@ -1,5 +1,31 @@
+import { statistics} from "@/app/data"
 export default function Sales() {
+    const from = statistics.from;
+    const to = statistics.to;
+    const data = statistics.data;
     return (
-        <div>sales</div>
+        <div>
+            <h1>sells</h1>
+            <p>from: {from}</p>
+            <p>to: { to}</p>
+            {data.map((item: Sell, index) => (<div key={index}>
+                <p>{item.itemId}</p>
+                <p>{item.name}</p>
+                <p>{item.value}</p>
+                <hr />
+            </div>))}
+        </div>
     )
+}
+
+type Statistic = {
+    from: string,
+    to: string,
+    data: Sell[]
+}
+
+type Sell = {
+    name: string,
+    itemId: string,
+    value: number
 }
---
+++ src/app/data.ts
@@ -0,0 +1,32 @@
+export const statistics = {
+    from: "2021-01-01",
+    to: "2021-01-01",
+    data: [
+        {
+            name: "manx",
+            itemId: "EST-90",
+            value: 30
+        },
+        {
+            name: "koi",
+            itemId: "EST-87",
+            value: 200
+        },
+        {
+            name: "Goldfish",
+            itemId: "EST-67",
+            value: 314
+        },
+        {
+            name: "Tiger Shard",
+            itemId: "EST-77",
+            value: 80
+        },
+        {
+            name: "Dolphin Shard",
+            itemId: "EST-88",
+            value: 200
+        },
+
+    ]
+} 
