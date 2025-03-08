b7190cd6aa2d8fb52cc9f64ccc92086e0b14f19c
ONeofSu <148432545+ONeofSu@users.noreply.github.com>
2025-03-08 15:49:49 +0800

    用户管理及订单管理后端全部完成

--- ManagePetStore-SSM-Dev/src/main/java/org/csu/petstore/service/OrderService.java
+++ ManagePetStore-SSM-Dev/src/main/java/org/csu/petstore/service/OrderService.java
@@ -5,9 +5,15 @@ import org.csu.petstore.vo.OrderVo;
 import java.util.List;
 
 public interface OrderService {
+    public List<OrderVo> getAllOrders();    //获得全部订单
+    public List<OrderVo> getCancelingOrders();  //获得全部待取消订单
+    public List<OrderVo> getCanceledOrders();   //获得全部已取消的订单
+    public List<OrderVo> getDoingOrders();  //获得进行中的订单
+    public List<OrderVo> getDoneOrders();   //获得已完成的订单
     public List<OrderVo> getUserOrders(String userId);  //获得用户的所有订单
     public List<OrderVo> getUserDoingOrders(String userId); //获得用户进行中的订单
     public List<OrderVo> getUserDoneOrders(String userId);  //获得用户已完成的订单
-    public List<OrderVo> getUserCancelingOrders(String userId); //获得用户正在取消的订单
+    public List<OrderVo> getUserCancelingOrders(String userId); //获得用户待取消的订单
     public List<OrderVo> getUserCanceledOrders(String userId);  //获得用户已取消的订单
+
 }
--- ManagePetStore-SSM-Dev/src/main/java/org/csu/petstore/service/impl/OrderServiceImpl.java
+++ ManagePetStore-SSM-Dev/src/main/java/org/csu/petstore/service/impl/OrderServiceImpl.java
@@ -27,6 +27,111 @@ public class OrderServiceImpl implements OrderService {
     LineItemService lineItemService;
 
     @Override
+    public List<OrderVo> getAllOrders() {
+        //init
+        List<Order> orders = orderMapper.selectList(new QueryWrapper<>());
+        List<OrderVo> orderVos = new ArrayList<>();
+
+        for(int i=0;i<orders.size();i++){
+            Order order = orders.get(i);
+            OrderVo orderVo = new OrderVo(order);
+
+            //get Status
+            OrderStatus orderStatus = orderStatusMapper.selectById(order.getOrderId());
+            if (orderStatus == null) {
+                orderStatus = new OrderStatus();
+                orderStatus.setOrderId(order.getOrderId());
+                orderStatus.setStatus("Error"); // 设置默认状态
+            }
+
+            orderVo.setStatus(orderStatus.getStatus());
+
+            //get order line items
+            orderVo.setLineItems(lineItemService.getLineItemsById(order.getOrderId()));
+
+            orderVos.add(orderVo);
+        }
+
+        return orderVos;
+    }
+
+    @Override
+    public List<OrderVo> getCancelingOrders() {
+        //init
+        List<OrderVo> orderVos = getAllOrders();
+        List<OrderVo> result = new ArrayList<>();
+
+        //choose status = W
+        for(int i=0;i<orderVos.size();i++){
+            OrderVo orderVo = orderVos.get(i);
+            String status = orderVo.getStatus();
+            if(status.equals("W")){
+                result.add(orderVo);
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    public List<OrderVo> getCanceledOrders() {
+        //init
+        List<OrderVo> orderVos = getAllOrders();
+        List<OrderVo> result = new ArrayList<>();
+
+        //choose status = N
+        for(int i=0;i<orderVos.size();i++){
+            OrderVo orderVo = orderVos.get(i);
+            String status = orderVo.getStatus();
+            if(status.equals("N")){
+                result.add(orderVo);
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    public List<OrderVo> getDoingOrders() {
+        //init
+        List<OrderVo> orderVos = getAllOrders();
+        List<OrderVo> result = new ArrayList<>();
+
+        //choose status = P
+        for(int i=0;i<orderVos.size();i++){
+            OrderVo orderVo = orderVos.get(i);
+            String status = orderVo.getStatus();
+            if(status.equals("P")){
+                result.add(orderVo);
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    public List<OrderVo> getDoneOrders() {
+        //init
+        List<OrderVo> orderVos = getAllOrders();
+        List<OrderVo> result = new ArrayList<>();
+
+        //choose status = F
+        for(int i=0;i<orderVos.size();i++){
+            OrderVo orderVo = orderVos.get(i);
+            String status = orderVo.getStatus();
+            if(status.equals("F")){
+                result.add(orderVo);
+            }
+        }
+
+        return result;
+    }
+
+    private static ArrayList<OrderVo> getResult() {
+        return new ArrayList<>();
+    }
+
+    @Override
     public List<OrderVo> getUserOrders(String userId) {
         //init
         List<OrderVo> orderVos = new ArrayList<OrderVo>();
