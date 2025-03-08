5da5bb6f03a3e9a24b992a057ac7eb14755748c1
lauvAri <198719993@qq.com>
2025-03-09 01:16:55 +0800

    add hero table and fix the react-date-picker issue

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,3 +1,49 @@
 export const springBoot = "http://localhost:8080/api"
 
 export const colors = ["#f87171", "#fca5a5", "#fcd34d", "#fde047", "#bef264", "#86efac", "#5eead4", "#93c5fd", "#fda4af"]
+
+export type ReturnOrder = {
+    orderId: string,
+    reason: string,
+    descn: string,
+    image: string,
+    status: string,
+}
+
+export type OrderVo = {
+    orderId: number,
+    userId: string,
+    orderDate: string,
+    shipAddress1: string,
+    shipAddress2: string,
+    shipCity: string,
+    shipState: string,
+    shipZip: string,
+    shipCountry: string,
+    billAddress1: string,
+    billAddress2: string,
+    billCity: string,
+    billState: string,
+    billZip: string,
+    billCountry: string,
+    courier: string,
+    totalPrice: number,
+    billToFirstName: string,
+    billToLastName: string,
+    shipToFirstName: string,
+    shipToLastName: string,
+    creditCard: string,
+    expiryDate: string,
+    cardType: string,
+    locale: string,
+    status: string,
+    lineItems: [LineItem]
+}
+
+export type LineItem = {
+    orderId: number,
+    lineNumber: number,
+    itemId: string,
+    quantity: number,
+    unitPrice: number
+}
--- src/app/dashboard/cancellations/page.tsx
+++ src/app/dashboard/cancellations/page.tsx
@@ -1,5 +1,80 @@
+'use client'
+import { springBoot } from "@/app/config";
+import { useEffect, useState } from "react";
+import { LXGW_WenKai_Mono_TC } from "next/font/google"
+import Link from "next/link";
+import { ReturnOrder, OrderVo } from '@/app/config'
+import {
+  Table,
+  TableHeader,
+  TableBody,
+  TableColumn,
+  TableRow,
+  TableCell,
+} from "@heroui/table";
+import React from "react";
+
+const geistSans = LXGW_WenKai_Mono_TC({
+    weight: '400',
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-geist-sans',
+})
+
+type Data = {
+    returnOrder: ReturnOrder,
+    orderVo: OrderVo,
+}[]
+
 export default function Cancellations() {
+    const [data, setData] = useState<Data>([]);
+    useEffect(() => {
+        async function fetchData() {
+            const resp = await fetch(`${springBoot}/refund/all`);
+            const nextData = await resp.json();
+            setData(nextData);
+        }
+        fetchData();
+    }, [data])
+    if (!data || data.length === 0) {
+        return <div>Loading...</div>
+    }
+
+    function showStatus(status: string) {
+        return status === 'W' ? 'Return Under Review' : status === 'N' ? 'Return Request Agreed' : "Return Request Refused"
+    }
+    function showStatusColor(status: string) {
+        return status === 'W' ? 'text-yellow-500' : status === 'N' ? 'text-green-500' : "text-red-500"
+    }
+    function showStatusOperation({ status, orderId } : OrderVo) {
+        return status === 'N' ? (<p>N\A</p>) : (<Link className="underline" href={`/dashboard/cancellations/${orderId}`}>details</Link>);
+    }
+
+
     return (
-        <div>Cancellations</div>
+        <Table isStriped>
+                <TableHeader>
+                    <TableColumn>Order ID</TableColumn>
+                    <TableColumn>Order Date</TableColumn>
+                    <TableColumn>Total Price</TableColumn>
+                    <TableColumn>User</TableColumn>
+                    <TableColumn>Status</TableColumn>
+                    <TableColumn>Operation</TableColumn>
+                </TableHeader>
+                <TableBody items={data}>
+                    {
+                        (item) => (
+                            <TableRow key={item.orderVo.orderId}>
+                                <TableCell className="text-center">{item.orderVo.orderId}</TableCell>
+                                <TableCell className="text-center">{item.orderVo.orderDate}</TableCell>
+                                <TableCell className="text-center">{item.orderVo.totalPrice}</TableCell>
+                                <TableCell className="text-center">{item.orderVo.userId}</TableCell>
+                                <TableCell className={`text-center ${showStatusColor(item.orderVo.status)}`}>{showStatus(item.orderVo.status)}</TableCell>
+                                <TableCell className="text-center">{ showStatusOperation(item.orderVo)}</TableCell>
+                            </TableRow>
+                        )
+                    }
+                </TableBody>
+            </Table>
     )
 }
