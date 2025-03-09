1c0b81dd8fa6d704408848b351a4390fc36901bf
lauvAri <198719993@qq.com>
2025-03-09 14:02:39 +0800

    添加文件上传接口和文件映射

--- MyPetStore-SSM-Dev/src/main/java/org/csu/petstore/MyPetStoreSsmDevApplication.java
+++ MyPetStore-SSM-Dev/src/main/java/org/csu/petstore/MyPetStoreSsmDevApplication.java
@@ -3,6 +3,11 @@ package org.csu.petstore;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @MapperScan("org.csu.petstore.persistence")
@@ -12,4 +17,26 @@ public class MyPetStoreSsmDevApplication {
         SpringApplication.run(MyPetStoreSsmDevApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000") // Next.js 默认端口
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
+    //配置资源映射
+    @Configuration
+    public class WebConfig implements WebMvcConfigurer {
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry.addResourceHandler("/images/**")
+                    .addResourceLocations("file:" + "upload/images");
+        }
+    }
 }
